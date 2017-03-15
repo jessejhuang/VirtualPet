fedcaf1631f91b8f4b059c1a933f2b2364817a47
Jesse Huang <jessehuang@wustl.edu>
2017-03-15 00:02:02 -0500

    Initial Commit

---
+++ JesseHuang-Lab5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JesseHuang-Lab5
+//
+//  Created by Jesse Huang on 3/15/17.
+//  Copyright © 2017 Jesse Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
